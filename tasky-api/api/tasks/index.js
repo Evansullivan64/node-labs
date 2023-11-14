5e1586511c1a7dad8b6ed520ed6d0f7b1d04489f
Evan Sullivan <20093137@mail.wit.ie>
2023-11-14 19:54:40 +0000

    Week 9 Lab exercise done

--- tasky-api/api/tasks/index.js
+++ tasky-api/api/tasks/index.js
@@ -16,14 +16,16 @@ router.get('/:id', (req, res) => {
     return res.status(200).json(task);
 });
 router.post('/', (req, res) => {
-    const { title, description, deadline, priority, done } = req.body;
+    const { title, description, deadline, priority, done,created_at,updated_at } = req.body;
     const newTask = {
         id: uuidv4(),
         title,
         description,
         deadline,
         priority,
-        done
+        done,
+        created_at:new Date().toISOString(),
+        updated_at:new Date().toISOString()
     };
     tasksData.tasks.push(newTask);
     res.status(201).json(newTask);
@@ -36,7 +38,14 @@ router.put('/:id', (req, res) => {
     if (taskIndex === -1) {
         return res.status(404).json({ status: 404, message: 'Task not found' });
     }
-    const updatedTask = { ...tasksData.tasks[taskIndex], ...req.body, id:id };
+
+    const updatedTask = {
+        ...tasksData.tasks[taskIndex],
+        ...req.body,
+        updated_at: new Date().toISOString(), 
+        id: id
+    };
+
     tasksData.tasks[taskIndex] = updatedTask;
     res.json(updatedTask);
 });
