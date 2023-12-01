d936779cf16a3a3f1be9df56fed0ed7d3e811f4d
Evan Sullivan <20093137@mail.wit.ie>
2023-12-01 15:32:08 +0000

    Added Movies Route

--- movies-api/index.js
+++ movies-api/index.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 import usersRouter from './api/users';
 import './db';
+import moviesRouter from './api/movies';
 import defaultErrHandler from './errHandler'
 
 dotenv.config();
@@ -13,6 +14,7 @@ const port = process.env.PORT;
 app.use(cors());
 app.use(express.json());
 app.use('/api/users', usersRouter);
+app.use('/api/movies', moviesRouter);
 app.use(defaultErrHandler);
 
 app.listen(port, () => {
